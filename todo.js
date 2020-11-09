7e166bb45ef8768a930b1ec4c1a6e0df9f573e33
Amit Patil <amitgpatil215@gmail.com>
2020-11-10 00:47:03 +0530

    Adding & removing todos

--- todo.js
+++ todo.js
@@ -6,12 +6,12 @@
 /// adding myvalue into h1 tag
 //h.append(myValue)
 /// h1 tag got selected
-/// appended our h1 tah
+/// appended our h tag after after h1 tag
 //document.querySelector('h1').appendChild(h);
 
 
 var ul = document.getElementById('list')
-
+var li;
 
 // selecting add button
 var addButton = document.getElementById('add')
@@ -22,7 +22,52 @@ var removeButton = document.getElementById('remove')
 removeButton.addEventListener('click', removeItem)
 
 function addItem() {
-    console.log('Add Button Clicked');
+    // selecting textfield
+    var input = document.getElementById('input')
+    // finding inputed value
+    var inputValue = input.value;
+    ul = document.getElementById('list');
+    var textNode = document.createTextNode(inputValue)
+
+    if (inputValue === '') {
+        var p = document.createElement('p')
+        var pValue = document.createTextNode('Enter your todo')
+        p.append(pValue)
+        document.querySelector('input').append(p);
+        console.log('ok');
+    } else {
+        // creating list
+        li = document.createElement('li');
+        // creating input
+        var checkbox = document.createElement('input')
+        // assiging input  checkbox as type
+        checkbox.type = 'checkbox';
+        // setting it an id
+        checkbox.setAttribute('id', 'check')
+
+        // creating label
+        var label = document.createElement('label');
+        label.setAttribute('for', 'item')
+
+        //add these elements on web page
+
+        // generating elements
+        ul.appendChild(label);
+        li.appendChild(checkbox);
+        //putting textnode inside label
+        label.appendChild(textNode);
+        li.appendChild(label);
+        // inserting at the top of the list
+        ul.insertBefore(li, ul.childNodes[0]);
+
+        //For trasition
+        setTimeout(() => {
+            li.className = 'visual';
+        }, 3);
+
+        //clearing the value
+        input.value = ''
+    }
 }
 
 function removeItem() {
